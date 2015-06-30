74710ccfe1a9bc27d644152109fe7eed4463515a
gvenvivar <gven.inc@gmail.com>
2015-06-30 16:35:42 +0300

    0.1

---
+++ js/common.js
@@ -0,0 +1,115 @@
+$(document).ready(function() {
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				
+				$("#form").trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	
+
+	//resize fullscreen img
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$('.parallax-window').parallax({
+		imageSrc: '../_optimized_html/img/bg3.jpg' 
+	});
+
+	//menu click
+	$(".toggle_menu").click(function() {
+	  $(".sandwich").toggleClass("active");
+	});
+
+	$(".nav-menu ul a").click(function() {
+		$(".nav-menu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".name-wrap").css("opacity", "1");
+	});
+	$(".toggle_menu").click(function() {
+		if ($(".nav-menu").is(":visible")) {
+			$(".name-wrap").css("opacity", "1");
+			$(".nav-menu").fadeOut(600);
+			$(".nav-menu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".name-wrap").css("opacity", ".1");
+			$(".nav-menu").fadeIn(600);
+			$(".nav-menu li a").addClass("fadeInUp animated");
+		};
+	});
+
+	//animation
+
+	// if ($(window).width() > 1024) {
+	// 	$(".animation_1").animated("flipInY");
+	// 	$(".animation_2").animated("fadeInLeft");
+	// 	$(".animation_3").animated("fadeInRight");
+	// }
+
+
+	//active
+	$(".paint-nav li").click(function() {
+		$(".paint-nav li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	//form validation
+	//$("input, select, textarea").jqBootstrapValidation();
+	
+	//tabs
+	$(".tab_item").not(":first").hide();
+	$(".wrapper .tab").click(function() {
+		$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	//scroll to
+	$(".nav-menu ul a").mPageScroll2id();
+	$(".top-nav ul a").mPageScroll2id();
+
+
+});
+
+
